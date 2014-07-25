7f2e90b0317f5d9f229f6e4cecef43378f16ba79
TheRoks <stefan@theroks.com>
2014-07-25 21:28:30 +0200

    docu

---
+++ site/js/docs-setup.js
@@ -0,0 +1,33 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "dutchPublicHolidays",
+      "shortName": "dutchPublicHolidays",
+      "type": "overview",
+      "moduleName": "dutchPublicHolidays",
+      "shortDescription": "dutchPublicHolidays",
+      "keywords": "add angular api app dependency dutch-public-holidays dutchpublicholidays html include included js ll main min module modules myapp needed ng-app organization overview purposes script src sub-modules umbrella var"
+    },
+    {
+      "section": "api",
+      "id": "dutchPublicHolidays.dutchPublicHolidays",
+      "shortName": "dutchPublicHolidays.dutchPublicHolidays",
+      "type": "object",
+      "moduleName": "dutchPublicHolidays",
+      "shortDescription": "dutchPublicHolidays service is responsible for representing states as well as transitioning",
+      "keywords": "angular api app calculation coming current day defines determines dutch dutchpublicholidays friday function getholidays good holiday holidays included includegoodfriday includeliberationday includeliberationdayonlustrum interfaces isholiday liberation lustrum module object objects public representing responsible returns service transitioning true year"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
